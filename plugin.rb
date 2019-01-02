c8c72e3ee42d4b88c73d33a45829506942635eb1
sau226 <32805612+sau226@users.noreply.github.com>
2019-01-02 16:52:50 +0800

    Enable Discourse dependency

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,7 @@
 enabled_site_setting :ip_override_plugin_enabled
 
 after_initialize do
+  require_dependency 'discourse'  
   require_dependency 'rack'
   load File.expand_path('../lib/discourse_ip_override/modify.rb', __FILE__)
 end
